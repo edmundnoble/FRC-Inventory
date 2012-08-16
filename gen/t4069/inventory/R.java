4e23b286a2b4d8f3fb96a71b3c6159528a878e23
Edmund Noble <edmundnoble@gmail.com>
2012-08-16 17:54:15 -0400

    Added add dialog.

--- gen/t4069/inventory/R.java
+++ gen/t4069/inventory/R.java
@@ -26,11 +26,27 @@ public final class R {
         public static final int button4=0x7f080005;
         public static final int button5=0x7f080006;
         public static final int button6=0x7f080004;
+        public static final int editText1=0x7f080009;
+        public static final int editText2=0x7f08000c;
+        public static final int editText3=0x7f08000f;
         public static final int listView1=0x7f080003;
-        public static final int menu_settings=0x7f080007;
+        public static final int menu_settings=0x7f080013;
+        public static final int spinner1=0x7f080012;
+        public static final int tableRow1=0x7f080007;
+        public static final int tableRow2=0x7f08000a;
+        public static final int tableRow3=0x7f08000d;
+        public static final int tableRow4=0x7f080010;
+        public static final int textView1=0x7f080008;
+        public static final int textView2=0x7f08000b;
+        public static final int textView3=0x7f08000e;
+        public static final int textView4=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int add_dialog=0x7f030001;
+        public static final int edit_dialog=0x7f030002;
+        public static final int filter_dialog=0x7f030003;
+        public static final int sign_out_dialog=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
--- src/t4069/inventory/MainActivity.java
+++ src/t4069/inventory/MainActivity.java
@@ -1,17 +1,25 @@
 package t4069.inventory;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
@@ -21,38 +29,49 @@ public class MainActivity extends Activity {
 			signOutButton, editButton;
 	private ListView partView;
 	private Dialog filter, add, signOut, edit;
-	private CharSequence[] categories = {"No category"};
-private OnClickListener buttonListener = new OnClickListener() {
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.button1: {
-			add();
-			break;}
-		case R.id.button2: {
-			edit();
-			break;}
-		case R.id.button3: {
-			signOut();
-			break;}
-		case R.id.button4: {
-			remove();
-			break;}
-		case R.id.button5: {
-			filter();
-			break;}
-		case R.id.button6: {
-			resetFilter();
-			break;}
+	private static final int ADD = 1, SIGNOUT = 2, EDIT = 3, FILTER = 4;
+	private ArrayList<String> categories = new ArrayList<String>();
+	private ArrayList<Part> parts = new ArrayList<Part>();
+	private ArrayList<String> visibleParts = new ArrayList<String>();
+	private OnClickListener buttonListener = new OnClickListener() {
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.button1: {
+				add();
+				break;
+			}
+			case R.id.button2: {
+				edit();
+				break;
+			}
+			case R.id.button3: {
+				signOut();
+				break;
+			}
+			case R.id.button4: {
+				remove();
+				break;
+			}
+			case R.id.button5: {
+				filter();
+				break;
+			}
+			case R.id.button6: {
+				resetFilter();
+				break;
+			}
+			}
 		}
-	}
-};
+	};
 
-DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
-	
-	public void onClick(DialogInterface dialog, int which) {
+	DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int which) {
+			if (dialog.equals(add)) {
+				AlertDialog addDialog = (AlertDialog) add;
 
-	}
-};
+			}
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -66,18 +85,56 @@ DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickList
 		filterButton = (Button) findViewById(R.id.button5);
 		resetFilterButton = (Button) findViewById(R.id.button6);
 		makeDialogs();
+		addButton.setOnClickListener(buttonListener);
+		removeButton.setOnClickListener(buttonListener);
+		editButton.setOnClickListener(buttonListener);
+		signOutButton.setOnClickListener(buttonListener);
+		filterButton.setOnClickListener(buttonListener);
+		resetFilterButton.setOnClickListener(buttonListener);
 	}
 
-	protected void add() {
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case ADD: return add; 
+		case EDIT: return edit; 
+		case SIGNOUT: return signOut;
+		case FILTER: return filter;
+		default: return null;
+		}
+	}
+
+	private void makeDialogs() {
+		LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+				.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.add_dialog, null);
+		add = new Dialog(this);
+		add.setTitle("Add Part");
+		add.setContentView(layout);
+		Spinner addCategories = (Spinner) layout.findViewById(R.id.spinner1);
+		addCategories.setAdapter((new ArrayAdapter<String>(
+				getApplicationContext(),
+				android.R.layout.simple_expandable_list_item_1, categories)));
+		if (add == null || layout == null) Log.d("FRC INVENTORY", "NULL!");
 		
+
+	}
+
+	String name;
+	String partNumber;
+	String quantity;
+	String storedQuantity;
+	String category;
+
+	protected void add() {
+		add.show();
 	}
 
 	protected void edit() {
-		
+		edit.show();
 	}
 
 	protected void signOut() {
-		
+		signOut.show();
 	}
 
 	protected void remove() {
@@ -85,11 +142,11 @@ DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickList
 	}
 
 	protected void resetFilter() {
-		
+
 	}
 
 	protected void filter() {
-		
+		filter.show();
 	}
 
 	@Override
@@ -98,18 +155,38 @@ DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickList
 		return true;
 	}
 
-	private void makeDialogs() {
-		final CharSequence[] items = {"Red", "Green", "Blue"};
+	class Part {
+		String name;
+		String number;
+		String manufacturer;
+		String category;
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Pick a color");
-		builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
-		    public void onClick(DialogInterface dialog, int item) {
-		        Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
-		    }
-		});
-		AlertDialog filter = builder.create();
+		public Part(String name, String number, String manufacturer,
+				String category) {
+			this.name = name;
+			this.number = number;
+			this.manufacturer = manufacturer;
+			this.category = category;
+		}
+
+		public void setStr(String prop, String val) {
+			if (prop.equalsIgnoreCase("name"))
+				name = val;
+			else if (prop.equalsIgnoreCase("number"))
+				number = val;
+			else if (prop.equalsIgnoreCase("manufacturer"))
+				manufacturer = val;
+			else if (prop.equalsIgnoreCase("category")) {
+				String category = new String(val);
+				for (String cat : categories) {
+					if (category.equals(cat))
+						this.category = cat;
+				}
+			} else
+				throw new IllegalArgumentException("Property " + prop
+						+ " doesn't exist!");
+		}
+
 	}
-	
-	
+
 }
